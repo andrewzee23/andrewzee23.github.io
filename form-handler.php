dd88d2a87e3335b306f622071931a3edc6495253
andrewzee23 <andrewazamora@gmail.com>
2021-01-14 16:19:35 -0600

    testing email function on form

---
+++ form-handler.php
@@ -0,0 +1,24 @@
+<?php
+    $name = $_POST['name'];
+    $vistor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+
+    $email_from = 'andrewazamoraSUBMISSION@gmail.com';
+
+    $email_subject = 'New Form Submission';
+
+    $email_body = 'User Name: $name.\n'.
+                    'User Email: $vistor_email.\n'.
+                        'User Message: $message.\n';
+
+    $to = 'andrewazamora@gmail.com';
+
+    $headers = 'From: $email_from \r\n';
+
+    $headers = 'Reply-To: $vistor_email \r\n';
+
+    mail($to, $email_subject, $email_body, $headers);
+
+    header('Location: index.html');
+?>
